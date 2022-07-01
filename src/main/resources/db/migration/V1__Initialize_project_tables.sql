d8909c41e35c1cbbf0320d737e92b352f7c7e7f7
toughchb <ggang2000@gmail.com>
2022-07-02 08:45:40 +0900

    Revert "Step3 - 의존성 리팩터링 (#526)"
    This reverts commit 99e5da32c009453511fcb9c5818939af9c0a8eb2.

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -19,7 +19,6 @@ CREATE TABLE menu (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(19, 2) NOT NULL,
     menu_group_id BIGINT(20) NOT NULL,
-    version BIGINT(20) NOT NULL,
     PRIMARY KEY (id)
 );
 
